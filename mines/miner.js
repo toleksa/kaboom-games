8e2eb8a99af5a302e94f96641b8b34216e43ffa1
root <root@centos1.ac>
2021-07-25 12:45:02 +0200

    images

--- mines/miner.js
+++ mines/miner.js
@@ -5,3 +5,19 @@ kaboom({
   debug: true,
   clearColor: [0.5, 0.5, 0.5, 1],
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+  const cardArray = [
+    {
+      name: 'new',
+      img: 'images/new.png'
+    },
+    {
+      name: 'empty',
+      img: 'images/empty.png'
+    }
+  ]
+
+  const grid = document.querySelector('.grid')
+  const resultDisplay = document.querySelector('#result')
