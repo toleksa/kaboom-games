baa658d928f18845f1aef0407977bc32116ad84e
root <root@centos1.ac>
2021-07-25 23:14:28 +0200

    v1.0.0

--- mines/mines.js
+++ mines/mines.js
@@ -1,42 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const typesArray = [
-    {
-      name: 'new',
-      img: 'img/new.png'
-    },
-    {
-      name: 'empty',
-      img: 'img/empty.png'
-    },
-    {
-      name: 'mine',
-      img: 'img/mine.png'
-    },
-    {
-      name: 'flag',
-      img: 'img/flag.png'
-    },
-    {
-      name: '1',
-      img: 'img/1.png'
-    },
-  ]
-
-  const board=[]
-
   const grid = document.querySelector('.grid')
   const minesDisplay = document.querySelector('#mines')
-  const timerDisplay = document.querySelector('#timer')
-  let mines = 10
-  let minesLeft = mines
+  var sizeX = 10
+  var sizeY = 10
+  var mines = 10
+  var minesLeft = mines
+  var gameState = 'play'
+
+  function randomInt(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
 
   function createBoard(x,y,mines){
     for (let i=0; i<x; i++){
       for (let j=0; j<y; j++){
         const field = document.createElement('img')
         field.setAttribute('src','img/new.png')
-        field.setAttribute('x',i)
-        field.setAttribute('y',j)
         field.setAttribute('coord',i + "x" + j)
         field.setAttribute('type','new')
         field.setAttribute('counter',0)
@@ -45,53 +24,156 @@ document.addEventListener('DOMContentLoaded', () => {
         grid.appendChild(field)
       }
     }
-    for(let i=0; i<mines; i++){
-      let coord = i + 'x' + i
-      let newMine = document.querySelector("[coord=" + CSS.escape(coord) + "]")
-      newMine.setAttribute('type','mine')
+    //deploy mines
+    var counter = 0
+    while(true){
+      var coord = randomInt(0,sizeX-1) + 'x' + randomInt(0,sizeY-1)
+      var field = document.querySelector("[coord=" + CSS.escape(coord) + "]")
+      if(field.getAttribute('type')!='mine'){
+        field.setAttribute('type','mine')
+        counter++
+      }
+      if(counter==mines){
+        break
+      }
     }
 
-    document.querySelector("[coord='9x1']").setAttribute('counter','1')
-    document.querySelector("[coord='9x2']").setAttribute('counter','2')
-    document.querySelector("[coord='9x3']").setAttribute('counter','3')
-    document.querySelector("[coord='9x4']").setAttribute('counter','4')
-    document.querySelector("[coord='9x5']").setAttribute('counter','5')
-    document.querySelector("[coord='9x6']").setAttribute('counter','6')
-    document.querySelector("[coord='9x7']").setAttribute('counter','7')
-    document.querySelector("[coord='9x8']").setAttribute('counter','8')
+    //calculate counter for each field (num of mines around)
+    for (let i=0; i<x; i++){
+      for (let j=0; j<y; j++){
+        var currField=document.querySelector("[coord=" + CSS.escape(i + 'x' + j) + "]")
+        var counter = 0
+        if(currField.getAttribute('type')=='mine'){
+          continue
+        }
+        for(var k=i-1; k<=i+1; k++){
+          if((k<0) || (k>=x)){
+            continue
+          } else {
+            for(var l=j-1; l<=j+1; l++){
+              if((l<0) || (l>=y)){
+                continue
+              } else {
+                if((k==x) && (l==y)){
+                  continue
+                }
+                var neighboor=document.querySelector("[coord=" + CSS.escape(k + 'x' + l) + "]")
+                if(neighboor.getAttribute('type')=='mine'){
+                  counter++
+                }
+              }
+            }
+          }
+        }
+        currField.setAttribute('counter',counter)
+      }
+    }
+  } // END createBoard(x,y,mines)
+
+  function checkNeighboors(coord){
+    var fields = coord.split('x')
+    var x = parseInt(fields[0])
+    var y = parseInt(fields[1])
+    for(var k=x-1; k<x+2; k++){
+      if((k<0) || (k>=sizeX)){
+        continue
+      } else {
+        for(var l=y-1; l<=y+1; l++){
+          if((l<0) || (l>=sizeY)){
+                continue
+          } else {
+            if((k==x) && (l==y)){
+              continue
+            }
+            var neighboor=document.querySelector("[coord=" + CSS.escape(k + 'x' + l) + "]")
+            var counter = neighboor.getAttribute('counter')
+            if(neighboor.getAttribute('type')=='new' && neighboor.getAttribute('src')!='img/flag.png'){
+              neighboor.setAttribute('type','field')
+              if(counter=='0'){
+                neighboor.setAttribute('src','img/empty.png')
+                checkNeighboors(neighboor.getAttribute('coord'))
+              } else {
+                neighboor.setAttribute('src','img/' + counter + '.png')
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  function checkVictory(){
+    if(minesLeft!=0){
+      return
+    }
+    var counter = 0
+    for(var x=0; x<sizeX; x++){
+      for(var y=0; y<sizeY; y++){
+        currentField = document.querySelector("[coord=" + CSS.escape(x + 'x' + y) + "]")
+        if(currentField.getAttribute('type')=='mine' && currentField.getAttribute('src')=='img/flag.png'){
+          counter++
+        }
+      }   
+    }
+    if(counter==mines){
+      gameState='won'
+      minesDisplay.textContent = 'You won :)'
+    }
   }
 
   function clickField(){
+    if(gameState!='play'){
+      return
+    }
+    if(this.getAttribute('src') == 'img/flag.png'){
+      return
+    }
+
     let type = this.getAttribute('type')
     if (type == 'new'){
       let counter = this.getAttribute('counter')
-      this.setAttribute('type','empty')
-      this.setAttribute('src','img/empty.png')
+      if(counter!='0'){
+        this.setAttribute('src','img/' + counter + '.png')
+      } else {
+        this.setAttribute('src','img/empty.png')
+        checkNeighboors(this.getAttribute('coord'))
+      }
+      this.setAttribute('type','field')
+      minesDisplay.textContent = minesLeft
     }
     if (type == 'mine'){
+      minesDisplay.textContent = 'Game Over'
       this.setAttribute('src','img/mine.png')
+      gameState='lost'
     }
-    minesDisplay.textContent = minesLeft
   }
 
   function clickFlag(e){
-    e.preventDefault();
-    if(this.getAttribute('type') == 'flag'){
-      this.setAttribute('type','new')
-      this.setAttribute('src','img/new.png')
-      minesLeft++
-      minesDisplay.textContent = minesLeft
+    e.preventDefault()
+    if(gameState!='play'){
       return
     }
-    if(this.getAttribute('src') == 'img/new.png'){
-      this.setAttribute('type','flag')
+    var type = this.getAttribute('type')
+    if((type == 'new' || type == 'mine') && this.getAttribute('src') == 'img/new.png'){
       this.setAttribute('src','img/flag.png')
       minesLeft--
       minesDisplay.textContent = minesLeft
+      if(minesLeft==0){
+        checkVictory()
+      }
       return 
+    }
+    if((type == 'new' || type == 'mine') && this.getAttribute('src') == 'img/flag.png'){
+      this.setAttribute('src','img/new.png')
+      minesLeft++
+      minesDisplay.textContent = minesLeft
+      if(minesLeft==0){
+        checkVictory()
+      }
+      return
     }
   }
 
-  createBoard(10,10,mines)
+  createBoard(sizeX,sizeY,mines)
 })
 
