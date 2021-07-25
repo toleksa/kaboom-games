dc14851276e823a74e10aa77791ed591527a3e02
root <root@centos1.ac>
2021-07-25 13:05:21 +0200

    remove kaboom

--- mines/miner.js
+++ mines/miner.js
@@ -1,13 +1,4 @@
-kaboom({
-  global: true,
-  fullscreen: true,
-  scale: 2,
-  debug: true,
-  clearColor: [0.5, 0.5, 0.5, 1],
-})
-
 document.addEventListener('DOMContentLoaded', () => {
-  //card options
   const cardArray = [
     {
       name: 'new',
@@ -16,11 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
     {
       name: 'empty',
       img: 'images/empty.png'
-    }
+    },
+    {
+      name: 'mine',
+      img: 'images/mine.png'
+    },
   ]
 
   const grid = document.querySelector('.grid')
   const minesDisplay = document.querySelector('#mines')
   const timerDisplay = document.querySelector('#timer')
 
+  function createBoard(x,y,mines){
+    for (let i=0; i<x; i++){
+      for (let j=0; j<y; j++){
+        const field = document.createElement('img')
+        field.setAttribute('src','img/new.png')
+        field.setAttribute('x',i)
+        field.setAttribute('y',j)
+        field.addEventListener('click', clickField)
+        grid.appendChild(field)
+      }
+    }
+  }
+
+  createBoard(10,10,10)
+})
 
