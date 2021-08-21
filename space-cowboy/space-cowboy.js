d8a197a56c5d12c0f5ec9ad84cc534657a4ce64a
toleksa <toleksa@gmail.com>
2021-08-21 11:13:13 +0200

    canvas

--- space-cowboy/space-cowboy.js
+++ space-cowboy/space-cowboy.js
@@ -1,10 +1,19 @@
+// vim: ts=2:sw=2
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid')
+  //const grid = document.querySelector('.grid')
   const control = document.querySelector('.control')
   const scoreDisplay = document.querySelector('#score')
 
   function randomInt(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  function draw() {
+    var canvas = document.getElementById('field');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      ctx.fillRect(0, 0, 400, 400);
+    }
   }
 
   function gameInit(){
@@ -15,11 +24,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function gameReset(){
+    draw()
     scoreDisplay.textContent = 1
     startTimer()
   }
 
   gameInit()
-
 })
 
