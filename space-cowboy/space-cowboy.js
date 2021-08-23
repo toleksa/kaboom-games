7a07546ec80bc007c5c0eec4414762116f25d9a5
toleksa <toleksa@gmail.com>
2021-08-23 11:13:13 +0200

    spawnEnemies()

--- space-cowboy/space-cowboy.js
+++ space-cowboy/space-cowboy.js
@@ -3,25 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
   //const grid = document.querySelector('.grid')
   const control = document.querySelector('.control')
   const scoreDisplay = document.querySelector('#score')
+  var iBad  = new Image()
+  iBad.src = 'img/bad.png'
+  var iGood = new Image()
+  iGood.src = 'img/good.png'
+  var canvas = document.getElementById('field');
+  var ctx = canvas.getContext('2d');
   var delay = 0
   var score = 0
 
-  function randomInt(min,max) {
+  function rand(min,max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  function spawnEnemies(){
+    x=rand(0,380)
+    y=rand(0,380)
+    ctx.drawImage(iBad,x,y,20,20) 
   }
 
   function startTimer(){
     timer = setInterval(function(){
       spawnEnemies()
     }, delay)
-  }
-
-  function draw() {
-    var canvas = document.getElementById('field');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
-      ctx.fillRect(0, 0, 400, 400);
-    }
   }
 
   function gameInit(){
@@ -34,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function gameReset(){
     score = 0
     delay = 1000
-    draw()
+    ctx.fillRect(0, 0, 400, 400);
     scoreDisplay.textContent = 1
     startTimer()
   }
